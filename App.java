802ceb512412aa74611d8caea7ea4fe199bbe926
Mohammed Matar <jmatar-sd@hotmail.com>
2015-03-08 01:24:45 +0300

    add some comments

--- App.java
+++ App.java
@@ -1,3 +1,13 @@
+/*
+$ cd ${project_folder}
+$ javac App.java
+$ java App $secounds
+# secounds is your secounds
+# for example $ java App 50391
+# please notice that >> calc(int m, String ops, int mm) ; 
+# method is just a simple calculator ps i'm using ArchLinux and i don't have any calculator & i'm very lazy to install it, so you can just ignore it.
+*/
+
 class App{
 	public static  void main(String... args) {
 		calcTime(Integer.parseInt(args[0]));
